600b71ec59bef1686725094eb727df8c99a6aee5
Javixo <jalvarez@plazaverde.pe>
2015-12-05 12:45:21 -0500

    copiado pirkasEntrada.dev

--- resources/ticket.php
+++ resources/ticket.php
@@ -10,12 +10,12 @@
 							$printer -> selectPrintMode();
 							$printer -> setJustification(Escpos::JUSTIFY_LEFT);
 							$printer -> feed();
-							$printer -> text("Ticket N°: 59");
+							$printer -> text("Ticket N°: 60");
 							$printer -> feed();
-							$printer -> text("Fecha y Hora: 2015-12-05 11:57:17\n");
+							$printer -> text("Fecha y Hora: 2015-12-05 12:09:38\n");
 							$printer -> text("------------------------------------------\n");
 							$printer -> text("Concepto: ");
-                            $printer -> text("Descuentos Autorizados a Colegios");
+                            $printer -> text("Ingreso General");
                             $printer -> feed();
                             $printer -> text("Precio Unit. S/.: ");
                             $printer -> text("3.00");
@@ -28,4 +28,4 @@
                             $printer -> feed();
 
 
-							$printer -> text("------------------------------------------\n");$printer -> feed();$printer -> setJustification(Escpos::JUSTIFY_CENTER);$printer -> text("¡Llene sus datos y deposite este ticket en las ánforas para el sorteo navideño!\n");$printer -> feed();$printer -> selectPrintMode();$printer -> text("Nombres/Rzon Soc.:__________________________________________________________________\n");$printer -> text("Direcc.:__________________________________\n");$printer -> text("DNI/RUC.:_________________________________\n");$printer -> text("Teléfono.:________________________________\n");$printer -> text("Correo.:__________________________________\n");$printer -> feed();$printer -> text("Fecha de Impr.: 05-12-2015 11:57:18\n");$printer -> text("<<No válido como documento contable>>\n");$printer -> feed();$printer -> cut();$printer -> close();
+							$printer -> text("------------------------------------------\n");$printer -> feed();$printer -> setJustification(Escpos::JUSTIFY_CENTER);$printer -> text("¡Llene sus datos y deposite este ticket en las ánforas para el sorteo navideño!\n");$printer -> feed();$printer -> selectPrintMode();$printer -> text("Nombres/Rzon Soc.:__________________________________________________________________\n");$printer -> text("Direcc.:__________________________________\n");$printer -> text("DNI/RUC.:_________________________________\n");$printer -> text("Teléfono.:________________________________\n");$printer -> text("Correo.:__________________________________\n");$printer -> feed();$printer -> text("Fecha de Impr.: 05-12-2015 12:09:38\n");$printer -> text("<<No válido como documento contable>>\n");$printer -> feed();$printer -> cut();$printer -> close();
